2fc9c790778c97e9d37f686655898b441061b8d2
Paweł Stawikowski <pawikoski@gmail.com>
2024-01-25 16:04:35 +0100

    Add verbose_name_plural option to Category model

--- backend/api/models.py
+++ backend/api/models.py
@@ -11,6 +11,12 @@ LANGS = (
 class Category(models.Model):
     name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
 
 class Question(models.Model):
     question_no = models.IntegerField()
