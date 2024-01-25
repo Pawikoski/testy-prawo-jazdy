3d928018020079408bc53570469d631e24eeeedd
Paweł Stawikowski <pawikoski@gmail.com>
2024-01-25 16:05:48 +0100

    Create script to save questions from an Excel file to database

---
+++ backend/api/management/commands/update_questions.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+from typing import Any
+from django.core.management.base import BaseCommand
+from api.models import Question, Category
+
+
+class Command(BaseCommand):
+    def handle(self, *args: Any, **options: Any) -> str | None:
+        xslx_url = "https://www.gov.pl/attachment/1e683ccd-6293-4656-9c16-fab2628b0c46"
+        df = pd.read_excel(xslx_url, sheet_name="Arkusz1")
+        languages = ["pl", "en", "de"]
+        for row in df.itertuples():
+            question_no = row[2]
+            multilang_data = {
+                "pl": {
+                    "question": row[3],
+                    "answer_a": row[4],
+                    "answer_b": row[5],
+                    "answer_c": row[6],
+                },
+                "en": {
+                    "question": row[7],
+                    "answer_a": row[8],
+                    "answer_b": row[9],
+                    "answer_c": row[10],
+                },
+                "de": {
+                    "question": row[11],
+                    "answer_a": row[12],
+                    "answer_b": row[13],
+                    "answer_c": row[14],
+                }
+            }
+            valid_answer = row[15]
+            media = row[16]
+            categories = [Category.objects.get_or_create(name=category)[0] for category in row[17].split(",")]  # Get or create categories from string "a,b,c"
+
+            source = row[18]
+            security_explenation = row[19]
+
+            for lang in languages:
+                try:
+                    question = Question.objects.get(question_no=question_no, language=lang)
+                    self.stdout.write(self.style.WARNING(f"Question {question_no} already exists"))
+                except Question.DoesNotExist:
+                    question = Question.objects.create(
+                        question_no=question_no,
+                        language=lang,
+                        text=multilang_data[lang]["question"],
+                        answer_a=multilang_data[lang]["answer_a"],
+                        answer_b=multilang_data[lang]["answer_b"],
+                        answer_c=multilang_data[lang]["answer_c"],
+                        correct_answer=valid_answer,
+                        media=media,
+                        question_source=source,
+                        security_explenation=security_explenation,
+                    )
+                    question.categories.set(categories)
+                    question.save()
+                    self.stdout.write(self.style.SUCCESS(f"Question {question_no} added"))
