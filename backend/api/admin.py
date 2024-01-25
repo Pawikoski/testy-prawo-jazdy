7a91402cff9b34b0dec7c6447ed1d10b3c7bdabc
Paweł Stawikowski <pawikoski@gmail.com>
2024-01-25 16:07:17 +0100

    Add QuestionAdmin to admin.py, register Category model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question, Category
 
-# Register your models here.
+
+admin.site.register(Category)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_no', 'language', 'text')
+    list_filter = ('language', 'categories')
+    search_fields = ('text',)
