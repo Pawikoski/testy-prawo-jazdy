8c6e6f3d2f193f97b55513e93752363a0c14bf6a
Paweł Stawikowski <pawikoski@gmail.com>
2024-01-28 18:38:33 +0100

    Update import statement for Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/header/Header';
-import Footer from './containers/Footer';
+import Footer from './components/footer/Footer';
 import './App.css';
 import { Outlet } from 'react-router-dom';
 
