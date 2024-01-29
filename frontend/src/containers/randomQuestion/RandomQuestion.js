6fe82432a681c67c1a8815fe7f4073be48f727cc
Paweł Stawikowski <pawikoski@gmail.com>
2024-01-29 18:41:08 +0100

    Add random question functionality

--- frontend/src/containers/randomQuestion/RandomQuestion.js
+++ frontend/src/containers/randomQuestion/RandomQuestion.js
@@ -1,9 +1,26 @@
-const RandomQuestion = () => {
-  return (
-    <div>
-      <h1>Random Question</h1>
-    </div>
-  );
+import { Navigate } from 'react-router-dom'
+import buildSlug from '../../functions/buildSlug';
+import { useEffect, useState } from 'react';
+
+const fetchRandomQuestion = (setSlug) => {
+  fetch('http://localhost:8000/api/questions/?language=pl&random=true')
+  .then(response => response.json())
+  .then(data => {
+    const q = data.results[0];
+    setSlug(buildSlug(q.text, q.question_no));
+  });
 }
- 
+
+
+const RandomQuestion = () => {
+  const [slug, setSlug] = useState('');
+  useEffect(() => {
+    fetchRandomQuestion(setSlug);
+  }, []);
+
+  return (
+    <Navigate replace to={slug} />
+  )
+}
+
 export default RandomQuestion;
