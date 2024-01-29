0bd4d9d2248b931b16d462bb8f3584260ddd173a
Paweł Stawikowski <pawikoski@gmail.com>
2024-01-29 12:28:29 +0100

    Add AddToAny social sharing functionality

---
+++ frontend/src/components/social/AddToAny.js
@@ -0,0 +1,18 @@
+const AddToAny = () => {
+  return (
+    <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+      <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+      <a class="a2a_button_facebook"></a>
+      <a class="a2a_button_copy_link"></a>
+      <a class="a2a_button_x"></a>
+      <a class="a2a_button_pinterest"></a>
+      <a class="a2a_button_wykop"></a>
+      <a class="a2a_button_reddit"></a>
+      <a class="a2a_button_whatsapp"></a>
+      <a class="a2a_button_facebook_messenger"></a>
+      <a class="a2a_button_linkedin"></a>
+    </div>
+  );
+}
+
+export default AddToAny;
