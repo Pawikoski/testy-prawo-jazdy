e759327ec13cc00ab461cbe3f08c8dd2527aee0d
Paweł Stawikowski <pawikoski@gmail.com>
2024-01-29 17:59:00 +0100

    Refactor AddToAny component to use className prop

--- frontend/src/components/social/AddToAny.js
+++ frontend/src/components/social/AddToAny.js
@@ -1,16 +1,15 @@
-const AddToAny = () => {
+const AddToAny = ({className}) => {
   return (
-    <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
-      <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
-      <a class="a2a_button_facebook"></a>
-      <a class="a2a_button_copy_link"></a>
-      <a class="a2a_button_x"></a>
-      <a class="a2a_button_pinterest"></a>
-      <a class="a2a_button_wykop"></a>
-      <a class="a2a_button_reddit"></a>
-      <a class="a2a_button_whatsapp"></a>
-      <a class="a2a_button_facebook_messenger"></a>
-      <a class="a2a_button_linkedin"></a>
+    <div className={"a2a_kit a2a_kit_size_32 a2a_default_style" + (className && " " + className)}>
+      <a className="a2a_button_facebook"></a>
+      <a className="a2a_button_copy_link"></a>
+      <a className="a2a_button_x"></a>
+      <a className="a2a_button_pinterest"></a>
+      <a className="a2a_button_wykop"></a>
+      <a className="a2a_button_reddit"></a>
+      <a className="a2a_button_whatsapp"></a>
+      <a className="a2a_button_facebook_messenger"></a>
+      <a className="a2a_button_linkedin"></a>
     </div>
   );
 }
