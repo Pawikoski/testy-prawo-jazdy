c8485e2448dad74453f42b81f9cb66a89e0679ad
Paweł Stawikowski <pawikoski@gmail.com>
2024-01-31 17:38:36 +0100

    Add CSRF token initialization in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,23 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import setCSRFToken from './functions/setCsrf';
+import Cookies from 'js-cookie';
 
 
 function App() {
+  useEffect(() => {
+    const cookies = Cookies.get();
+    if (!cookies.csrf_token) {
+      setCSRFToken();
+    }
+    return () => {};
+  }, []);
+
+
   return (
-    <div>
+    <div style={{ minHeight: "100vh" }} className="d-flex flex-column">
       <Header />
       <Outlet />
       <Footer />
