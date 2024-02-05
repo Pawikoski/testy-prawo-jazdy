a0916ccc1375c521aa287ee2f0d27b03268c8e85
Paweł Stawikowski <pawikoski@gmail.com>
2024-02-05 14:36:01 +0100

    Add dynamic question counter to CategoryLandingPage

--- frontend/src/containers/categoryLandingPage/CategoryLandingPage.js
+++ frontend/src/containers/categoryLandingPage/CategoryLandingPage.js
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDatabase, faHandshakeSimple, faIdCard } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 const Icon = ({ icon }) => {
@@ -16,6 +18,26 @@ const CategoryLandingPage = () => {
   let { category } = useParams();
   category = category.split('-')[1].toUpperCase();
 
+  const [questionsCount, setQuestionsCount] = useState(null);
+  const [dynamicCounter, setDynamicCounter] = useState(0);
+
+  useEffect(() => {
+    axios.get('/question-count', { params: { categories: category } })
+      .then(response => setQuestionsCount(response.data.count))
+      .catch(error => console.log(error));
+  }, [category]);
+
+  useEffect(() => {
+    if (questionsCount === null) {
+      const interval = setInterval(() => {
+        setDynamicCounter(prevCounter => prevCounter + 1);
+      }, 1);
+      return () => clearInterval(interval);
+    } else {
+      setDynamicCounter(questionsCount);
+    }
+  }, [questionsCount]);
+
   return (
     <Container>
       <h2 className="pb-2 border-bottom">Kategoria {category}</h2>
@@ -24,7 +46,7 @@ const CategoryLandingPage = () => {
         <Col className="d-flex align-items-start">
           <Icon icon={faDatabase} />
           <div>
-            <h2>{parseInt(Math.random() * 6000)} Pytań i odpowiedzi</h2>
+            <h2><span style={{width: "4ch", display: "inline-block"}}>{dynamicCounter}</span> Pytań i odpowiedzi</h2>
             <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
             <Button variant="primary">Przejdź do pytań kat. {category}</Button>
           </div>
